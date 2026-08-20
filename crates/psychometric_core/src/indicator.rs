f60748dbbdc922abcf6c2621801589ba855f8af2
Seongho Bae <me@seonghobae.me>
2026-08-20 22:20:33 +0900

    test(psychometric): close indicator branch coverage

--- crates/psychometric_core/src/indicator.rs
+++ crates/psychometric_core/src/indicator.rs
@@ -134,8 +134,28 @@ mod tests {
     fn valid_kinds_pass_and_zero_right_variance_is_singular() {
         require_valid_indicator(IndicatorKind::IsometricLogRatio).expect("ilr");
         assert_eq!(
+            require_valid_indicator(IndicatorKind::RawProportion),
+            Err(PsychometricError::RawProportionForbidden)
+        );
+        assert_eq!(
             pearson_correlation(&[1.0, 2.0], &[3.0, 3.0], IndicatorKind::LogisticNormal),
             Err(PsychometricError::SingularDesign)
+        );
+        assert_eq!(
+            pearson_correlation(&[1.0, 1.0], &[2.0, 3.0], IndicatorKind::LogisticNormal),
+            Err(PsychometricError::SingularDesign)
+        );
+        assert_eq!(
+            pearson_correlation(&[1.0, 2.0], &[1.0], IndicatorKind::LogisticNormal),
+            Err(PsychometricError::InvalidNumericInput)
+        );
+        assert_eq!(
+            pearson_correlation(&[f64::NAN, 2.0], &[1.0, 2.0], IndicatorKind::LogisticNormal),
+            Err(PsychometricError::InvalidNumericInput)
+        );
+        assert_eq!(
+            pearson_correlation(&[1.0, 2.0], &[1.0, f64::NAN], IndicatorKind::LogisticNormal),
+            Err(PsychometricError::InvalidNumericInput)
         );
         assert_eq!(
             pearson_correlation(
